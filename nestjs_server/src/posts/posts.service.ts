ff2d285547d4c1a661aeff4c5352d0eff7d2cc76
미플 <devmeeple@gmail.com>
2023-11-02 18:27:06 +0900

    feat: Save 함수로 업데이트 하기
    - 데이터가 존재하지 않는다면 새로 생성
    - 데이터가 존재한다면 값을 업데이트

--- nestjs_server/src/posts/posts.service.ts
+++ nestjs_server/src/posts/posts.service.ts
@@ -86,13 +86,19 @@ export class PostsService {
             commentCount: 0,
         });
 
-        const newPost = await this.postsRepository.save(post);
-
-        return newPost;
+        return await this.postsRepository.save(post);
     }
 
-    updatePost(postId: number, author: string, title: string, content: string) {
-        const post = posts.find((post) => post.id === postId);
+    async updatePost(postId: number, author: string, title: string, content: string) {
+        // save의 기능
+        // 1) 만약에 데이터가 존재하지 않는다면 (id 기준으로) 새로 생성한다.
+        // 2) 만약에 데이터가 존재한다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트 한다.
+
+        const post = await this.postsRepository.findOne({
+            where: {
+                id: postId,
+            },
+        });
 
         if (!post) {
             throw new NotFoundException();
@@ -110,9 +116,7 @@ export class PostsService {
             post.content = content;
         }
 
-        posts = posts.map((prevPost) => prevPost.id === postId ? post : prevPost);
-
-        return post;
+        return await this.postsRepository.save(post);
     }
 
     deletePost(postId: number) {
