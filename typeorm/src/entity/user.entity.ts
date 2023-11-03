8eb0c4dcefad6d50e6df872e815138a6482f3103
미플 <devmeeple@gmail.com>
2023-11-04 03:34:55 +0900

    feat: Column Property 정리하기
    - type
    - name
    - length
    - nullable
    - update
    - select
    - default
    - unique

--- typeorm/src/entity/user.entity.ts
+++ typeorm/src/entity/user.entity.ts
@@ -28,7 +28,30 @@ export class UserModel {
     id: number;
 
     // 제목
-    @Column()
+    @Column({
+        // 데이터베이스에서 인지하는 칼럼 타입
+        // 자동으로 유추됨, 특정한 값을 넣고 싶으면 선언가능
+        type: 'varchar',
+        // 데이터베이스 칼럼 이름
+        // 프로퍼티 이름으로 자동유추됨
+        name: 'title',
+        // 값의 길이
+        // 입력할 수 있는 글자의 길이가 300
+        length: 300,
+        // null이 가능한지
+        nullable: true,
+        // true면 처음 저장할 때만 값 지정 가능
+        // 이후에는 값 변경 불가능,
+        update: true,
+        // find()를 실행할때 기본으로 값을 불러올지
+        // 기본값이 true
+        select: false,
+        // 기본값
+        // 아무것도 입력하지 않았을때 기본으로 입력되게 되는 값
+        default: 'default value',
+        // 칼럼중에서 유일무이한 값이 돼야하는지, 이메일에 사용
+        unique: true,
+    })
     title: string;
 
     /**
