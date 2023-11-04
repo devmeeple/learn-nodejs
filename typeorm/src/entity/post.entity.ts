47a6eacd418be0afdf83c5e9912953d5b5e2871f
미플 <devmeeple@gmail.com>
2023-11-04 21:27:22 +0900

    feat: Many to Many Relationship 구현해보기
    - 다대다 N:M

--- typeorm/src/entity/post.entity.ts
+++ typeorm/src/entity/post.entity.ts
@@ -1,5 +1,6 @@
-import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
 import {UserModel} from './user.entity';
+import {TagModel} from './tag.entity';
 
 @Entity()
 export class PostModel {
@@ -9,6 +10,10 @@ export class PostModel {
 
     @ManyToOne(() => UserModel, (user) => user.posts)
     author: UserModel;
+
+    @ManyToMany(() => TagModel, (tag) => tag.posts)
+    @JoinTable()
+    tags: TagModel[];
 
     @Column()
     title: string;
