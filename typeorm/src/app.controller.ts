c8a5c72ea0c1dabeadc137ef7c299dfc3f5eed37
미플 <devmeeple@gmail.com>
2023-11-05 01:20:41 +0900

    feat: FindManyOptions 파라미터 알아보기
    - select
    - where
    - relations
    - order
    - skip
    - take

--- typeorm/src/app.controller.ts
+++ typeorm/src/app.controller.ts
@@ -22,12 +22,47 @@ export class AppController {
 
   @Post('users')
   postUser() {
-    return this.userRepository.save({});
+    return this.userRepository.save({
+      email: '1234@gmail.com',
+    });
   }
 
   @Get('users')
   getUsers() {
-    return this.userRepository.find({});
+    return this.userRepository.find({
+      // 어떤 프로퍼티를 선택할지
+      // 기본은 모든 프로퍼티를 가져온다.
+      // 만약에 select를 정의하지 않으면
+      // select를 정의하면 정의된 프로퍼티들만 가져오게 된다.
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        version: true,
+        profile: {
+          id: true,
+        }
+      },
+      // 필터링할 조건을 입력하게 된다.
+      where: {
+        // profile: {
+        //   id: 3,
+        // }
+      },
+      // 관계를 가져오는법
+      relations: {
+        profile: true,
+      },
+      // 오름차 내림차
+      // ASC
+      // DESC
+      order: {
+        id: 'DESC',
+      },
+      // 처음 몇개를 제외할지,
+      skip: 0,
+      take: 2,
+    });
   }
 
   @Patch('users/:id')
@@ -42,6 +77,7 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
+      email: user.email + '0',
     });
   }
 
