910b5ef898f203525c3e559a6af518525f8958f3
미플 <devmeeple@gmail.com>
2023-12-29 21:14:50 +0900

    feat: 오름차순, 내림차순 지원 커서 페이지네이션 구현
    - 페이지 링크 추가
    - 분기 처리

--- conscious-practice/cf_sns/src/main.ts
+++ conscious-practice/cf_sns/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
     new ValidationPipe({
       // 페이지네이션 구현 시 쿼리에 요청(예: 정렬방법)이 들어오지 않아도 기본값을 할당하기 위해 옵션 추가
       transform: true,
+      // 자료형 데코레이터를 기준으로 변환 할 수 있도록 옵션 추가 class-validation과 함께 사용
+      transformOptions: {
+        enableImplicitConversion: true, // 암묵적으로 형 변환
+      },
     }),
   );
   await app.listen(3000);
