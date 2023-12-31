dd75fc8e54b8802da1912e7dfe4c0e7738601b0e
TaeGeun Jang <devmeeple@gmail.com>
2023-12-31 15:01:07 +0900

    feat: middleware 적용 (#50)

---
+++ conscious-practice/cf_sns/src/common/middleware/log.middleware.ts
@@ -0,0 +1,11 @@
+import { NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
+
+export class LogMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction): any {
+    console.log(
+      `[REQ] ${req.method} ${req.url} ${new Date().toLocaleString('kr')}`,
+    );
+    next();
+  }
+}
