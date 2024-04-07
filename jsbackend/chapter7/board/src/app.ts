98059334b55eed31319dd134e82e4af0b0d44a53
미플 <devmeeple@gmail.com>
2024-04-08 00:06:28 +0900

    feat: 글쓰기 UI 구현

--- jsbackend/chapter7/board/src/app.ts
+++ jsbackend/chapter7/board/src/app.ts
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.render('home', { title: '테스트 게시판', message: '만나서 반갑습니다' });
 });
 
+app.get('/write', (req, res) => {
+  res.render('write', { title: '테스트 게시판' });
+});
+
 app.listen(port, () => {
   console.log(`[server]: 서버 실행 http://localhost:${port}`);
 });
